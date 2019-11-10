9d9ad2433db4d555c374afab01d2769b476801dd
dany <danykaram97@gmail.com>
2019-11-10 16:05:53 -0400

    Adding folders MVC 3

---
+++ Controllers/Controlador.h
@@ -0,0 +1,48 @@
+#ifndef CONTROLADOR_H
+#define CONTROLADOR_H
+
+#include "../Librerias/Genericas/VGeneral.h"
+#include "../Librerias/Lista/Lista.cpp"
+//#include "../Librerias/Pila/Pila.cpp"
+//#include "../Librerias/Cola/Cola.cpp"
+#include "../Modelos/MTablaMultiplicar.h"
+#include <vector>
+
+class Controlador {
+private:
+    VGeneral  vg;
+    Lista<int> Lista1;
+    Pila<int> PilaA;
+    Cola<int> Cola1;
+
+   // Pila<int> PilaB;
+   // Cola<Pila<int>> Cola2;
+    MMultiplicador mmul; //pila
+    MTablaMultiplicar mtab; //cola
+
+  bool check;
+  // int nro;
+   vector<int> vecnro;
+public:
+    Controlador();
+    void CargarLista();
+    void ImprimirLista();
+    void CargarPila();
+    void ImprimirPila();
+    void CargarCola();
+    void ImprimirCola();
+    void CargarTabla();
+    void ImprimirTabla();
+    void BorrarTabla();
+    void OpcionLista();
+    void OpcionPila();
+    void OpcionCola();
+    void OpcionTabla();
+    void OpcionAyuda();
+    void OpcionSalir();
+    void Menu();
+   //bool SearchTable(int q);
+  // int Busqueda(int n);
+};
+
+#endif
