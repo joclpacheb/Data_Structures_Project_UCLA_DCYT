51587f251dff82bea185bce7b2e4a37ef40f64fa
joclpacheb <joclpacheb@hotmail.com>
2019-11-11 10:03:11 -0400

    Updated the MVehicul class and methods.

--- Models/MVehiculo.cpp
+++ Models/MVehiculo.cpp
@@ -1,5 +1,56 @@
-//
-// Created by dany on 11/10/19.
-//
-
 #include "MVehiculo.h"
+
+MVehiculo::MVehiculo(){
+}
+
+Pila <int> MVehiculo::GetPilaHoras() const{
+return PilaHoras;
+}
+
+void MVehiculo::InsertTime(int n){
+pilaHoras.Insertar(n);
+}
+
+void MVehiculo::RemoveTime(int n){
+    pilaHoras.Remover(n);
+}
+bool MVehiculo::Empty(){
+    return pilaHoras.Vacia();
+}
+
+int MVehiculo::GetTipo(){
+    return tipo;
+}
+float MVehiculo::GetTanque(){
+    return tipo;
+}
+int MVehiculo::GetEspacio(){
+    return espacio;
+}
+void  MVehiculo::SetTipo(int ti){
+    tipo=ti;
+}
+
+void MVehiculo::SetEspacio(int e){
+    espacio=e;
+}
+
+void MVehiculo::SetTanque (float ta){
+    tanque=ta;
+}
+
+int MMultiplicador::GetNum() {
+        return number;
+    }
+
+void MMultiplicador::SetNum(int num) {
+        number = num;
+    }
+void MMultiplicador::InsertNumber(int n){
+        StackMult.Insertar(n);
+    }
+void MMultiplicador::RemoveNumber(int &n){
+      StackMult.Remover(n);
+    }
+  bool MMultiplicador::Empty(){
+        return StackMult.Vacia();
--- Models/MVehiculo.h
+++ Models/MVehiculo.h
@@ -1,12 +1,32 @@
-//
-// Created by dany on 11/10/19.
-//
-
 #ifndef DATA_STRUCTURES_PROJECT_UCLA_DCYT_MVEHICULO_H
 #define DATA_STRUCTURES_PROJECT_UCLA_DCYT_MVEHICULO_H
 
+#include "../Librerias/Pila/Pila.cpp"
 
 class MVehiculo {
+
+private:
+    int tipo;
+    //el vehiculo puede ser de tipo vehiculo(normal), tipo transporte o tipo carga....
+    int espacio;
+    //en función de ello tiene un espacio que ocupa en la bahía en la que se encuentre...
+    float tanque;
+
+    Pila <int> pilaHoras;
+
+public:
+
+    Pila <int> GetPilaHoras() const;
+    MVehiculo(int tipo);
+    ~MVehiculo();
+    void InsertTime(int n) ;
+    void RemoveTime(int &n)bool Empty();
+    int GetTipo();
+    int GetEspacio();
+    float GetTanque();
+    void SetTipo(int ti);
+    void SetEspacio(int e);
+    void SetTanque (float ta);
 
 };
 
